3fae94a771a02d4dfd34bbc9e304cff4427a7501
himyjan <51815522+himyjan@users.noreply.github.com>
2022-11-01 19:00:37 +0800

    fix build

--- api/fb.ts
+++ api/fb.ts
@@ -1,3 +1,5 @@
+'use client';
+
 const fb = {
   loadScript() {
     return new Promise((resolve) => {
@@ -10,17 +12,17 @@ const fb = {
         if (d.getElementById(id)) return;
         js = d.createElement(s);
         js.id = id;
-        js.src = "https://connect.facebook.net/zh_TW/sdk.js";
+        js.src = 'https://connect.facebook.net/zh_TW/sdk.js';
         fjs.parentNode.insertBefore(js, fjs);
-      })(document, "script", "facebook-jssdk");
+      })(document, 'script', 'facebook-jssdk');
     });
   },
   init() {
     window.FB.init({
-      appId: "700590737403665",
+      appId: '700590737403665',
       cookie: true,
       xfbml: true,
-      version: "v10.0"
+      version: 'v10.0',
     });
   },
   getLoginStatus() {
@@ -36,7 +38,7 @@ const fb = {
         (response) => {
           resolve(response);
         },
-        { scope: "public_profile,email" }
+        { scope: 'public_profile,email' },
       );
     });
   },
@@ -46,7 +48,7 @@ const fb = {
         resolve();
       });
     });
-  }
+  },
 };
 
 export default fb;
