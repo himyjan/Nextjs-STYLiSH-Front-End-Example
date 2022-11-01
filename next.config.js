10df0d327bac78efebb8c168c496f4558e31463e
himyjan <51815522+himyjan@users.noreply.github.com>
2022-11-01 13:57:14 +0800

    update package version

--- next.config.js
+++ next.config.js
@@ -2,18 +2,9 @@
 const nextConfig = {
 	reactStrictMode: true, // Recommended for the `pages` directory, default in `app`.
 	swcMinify: true,
-	compiler: {
-		styledComponents: true,
-	},
-	images: {
-		domains: ["api.appworks-school.tw"],
-	},
 	experimental: {
 		// Required:
 		appDir: true,
-		images: {
-			unoptimized: true,
-		},
 	},
 };
 
