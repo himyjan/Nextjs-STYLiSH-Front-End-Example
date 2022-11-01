86352371b73e06fb6e1a5f52704a19241c4eed0b
himyjan <51815522+himyjan@users.noreply.github.com>
2022-11-01 16:52:56 +0800

    upload code

--- next.config.js
+++ next.config.js
@@ -1,11 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: true, // Recommended for the `pages` directory, default in `app`.
-	swcMinify: true,
-	experimental: {
-		// Required:
-		appDir: true,
-	},
+  reactStrictMode: true, // Recommended for the `pages` directory, default in `app`.
+  swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'api.appworks-school.tw',
+        port: '',
+        pathname: '/assets/**',
+      },
+    ],
+  },
+  experimental: {
+    // Required:
+    appDir: true,
+  },
 };
 
 module.exports = nextConfig;
