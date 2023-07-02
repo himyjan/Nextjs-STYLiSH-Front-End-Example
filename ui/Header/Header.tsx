5b115cc1cae9f76df021853e82b03440e991ffe9
himyjan <51815522+himyjan@users.noreply.github.com>
2023-07-02 23:43:35 +0800

    Update Header.tsx

--- ui/Header/Header.tsx
+++ ui/Header/Header.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useState, useContext, KeyboardEventHandler } from 'react';
+import { useEffect, useState, useContext, KeyboardEvent } from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
 import Link from 'next/link';
 
@@ -288,7 +288,7 @@ function Header() {
         ))}
       </CategoryLinks>
       <SearchInput
-        onKeyPress={(e: KeyboardEventHandler<HTMLInputElement>) => {
+        onKeyPress={(e: KeyboardEvent<HTMLInputElement>) => {
           if (e.key === 'Enter') {
             router.push(`/?keyword=${inputValue}`);
           }
